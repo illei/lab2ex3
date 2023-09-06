1c163e73486fa02bdb9fc6f4a567b26eb0fc9dfd
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 15:16:35 +0200

    Add project files.

---
+++ lab2ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            int numar = 0;
+            int suma = 0;
+            int numaratorParitate = 0;
+
+            for (int i = 0; i < counter; i++)
+            {
+                 numar = int.Parse(Console.ReadLine());
+                if (numar % 2 == 0 )
+
+                {
+                    suma = suma + numar;
+                    numaratorParitate++;
+                }
+            }
+            Console.WriteLine("Media aritmetica a numerelor  pare : " + suma/numaratorParitate);
+        }
+    }
+}
